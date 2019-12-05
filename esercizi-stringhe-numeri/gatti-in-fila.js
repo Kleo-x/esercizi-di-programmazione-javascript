5f5866f1e4fa504dffc776b3bb8db4ea9f95a581
Kleo-x <58572088+Kleo-x@users.noreply.github.com>
2019-12-05 22:01:01 +0100

    Update gatti-in-fila.js

--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -15,5 +15,10 @@
   Per l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su),
   utilizza Math.ceil (arrotonda sempre all'intero superiore).
 
-  http://www.imparareaprogrammare.it
+http://www.imparareaprogrammare.it
 */
+http://www.imparareaprogrammare.it
+var cats = 44
+var cats in every row = 4
+var row = 11
+var remaining cats = 0/*
